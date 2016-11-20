e9b9e6a8e3038b5aa4d19d76d6c879ba3b6fd268
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-11-20 15:35:07 +0200

    Lifted classes for transformers-0.4 version

--- src/Generics/SOP/BasicFunctors.hs
+++ src/Generics/SOP/BasicFunctors.hs
@@ -41,6 +41,18 @@ import qualified GHC.Generics as GHC
 
 import Data.Functor.Classes
 
+#if MIN_VERSION_base(4,9,0)
+#define LIFTED_CLASSES 1
+#else
+#if MIN_VERSION_transformers(0,5,0)
+#define LIFTED_CLASSES 1
+#else
+#if MIN_VERSION_transformers_compat(0,5,0) && !MIN_VERSION_transformers(0,4,0)
+#define LIFTED_CLASSES 1
+#endif
+#endif
+#endif
+
 -- | The constant type functor.
 --
 -- Like 'Data.Functor.Constant.Constant', but kind-polymorphic
@@ -63,7 +75,7 @@ instance Traversable (K a) where
   traverse _ (K x) = pure (K x)
 #endif
 
-
+#ifdef LIFTED_CLASSES
 instance Eq2 K where
     liftEq2 eq _ (K x) (K y) = eq x y
 instance Ord2 K where
@@ -82,16 +94,34 @@ instance (Read a) => Read1 (K a) where
     liftReadsPrec = liftReadsPrec2 readsPrec readList
 instance (Show a) => Show1 (K a) where
     liftShowsPrec = liftShowsPrec2 showsPrec showList
+#else
+instance (Eq a) => Eq1 (K a) where
+    eq1 (K x) (K y) = x == y
+instance (Ord a) => Ord1 (K a) where
+    compare1 (K x) (K y) = compare x y
+instance (Read a) => Read1 (K a) where
+    readsPrec1 = readsData $ readsUnary "K" K
+instance (Show a) => Show1 (K a) where
+    showsPrec1 d (K x) = showsUnary "K" d x
+#endif
 
 -- This have to be implemented manually
 instance (Eq a) => Eq (K a b) where
     K x == K y = x == y
 instance (Ord a) => Ord (K a b) where
     compare (K x) (K y) = compare x y
+{-
 instance (Read a) => Read (K a b) where
-    readsPrec = readsData $ readsUnaryWith readsPrec "K" K
+    readsPrec d r = readParen (d > 10) (\r' ->
+        [ (K x, t)
+        | ("K", s) <- lex r'
+        , (x, t) <- readsPrec 11 s
+        ]) r
+-}
 instance (Show a) => Show (K a b) where
-    showsPrec d (K x) = showsUnaryWith showsPrec "K" d x
+    showsPrec d (K x) = showParen (d > 10)
+        $ showString "K "
+        . showsPrec 11 x
 
 instance Monoid a => Applicative (K a) where
   pure _      = K mempty
@@ -131,6 +161,7 @@ instance Monad I where
   I x >>= f = f x
 
 
+#ifdef LIFTED_CLASSES
 instance Eq1 I where
     liftEq eq (I x) (I y) = eq x y
 instance Ord1 I where
@@ -140,6 +171,16 @@ instance Read1 I where
          readsUnaryWith rp "I" I
 instance Show1 I where
     liftShowsPrec sp _ d (I x) = showsUnaryWith sp "I" d x
+#else
+instance Eq1 I where
+    eq1 (I x) (I y) = x == y
+instance Ord1 I where
+    compare1 (I x) (I y) = compare x y
+instance Read1 I where
+    readsPrec1 = readsData $ readsUnary "I" I
+instance Show1 I where
+    showsPrec1 d (I x) = showsUnary "I" d x
+#endif
 
 instance (Eq a) => Eq (I a) where (==) = eq1
 instance (Ord a) => Ord (I a) where compare = compare1
@@ -165,6 +206,7 @@ instance (Functor f, Functor g) => Functor (f :.: g) where
 
 -- Instances of lifted Prelude classes
 
+#ifdef LIFTED_CLASSES
 instance (Eq1 f, Eq1 g) => Eq1 (f :.: g) where
     liftEq eq (Comp x) (Comp y) = liftEq (liftEq eq) x y
 
@@ -186,13 +228,50 @@ instance (Show1 f, Show1 g) => Show1 (f :.: g) where
         sp' = liftShowsPrec sp sl
         sl' = liftShowList sp sl
 
-
 instance (Eq1 f, Eq1 g, Eq a) => Eq ((f :.: g) a) where (==) = eq1
 instance (Ord1 f, Ord1 g, Ord a) => Ord ((f :.: g) a) where compare = compare1
 instance (Read1 f, Read1 g, Read a) => Read ((f :.: g) a) where readsPrec = readsPrec1
 instance (Show1 f, Show1 g, Show a) => Show ((f :.: g) a) where showsPrec = showsPrec1
+#else
+-- kludge to get type with the same instances as g a
+newtype Apply g a = Apply (g a)
+
+getApply :: Apply g a -> g a
+getApply (Apply x) = x
+
+instance (Eq1 g, Eq a) => Eq (Apply g a) where
+    Apply x == Apply y = eq1 x y
+
+instance (Ord1 g, Ord a) => Ord (Apply g a) where
+    compare (Apply x) (Apply y) = compare1 x y
+
+instance (Read1 g, Read a) => Read (Apply g a) where
+    readsPrec d s = [(Apply a, t) | (a, t) <- readsPrec1 d s]
+
+instance (Show1 g, Show a) => Show (Apply g a) where
+    showsPrec d (Apply x) = showsPrec1 d x
+
+instance (Functor f, Eq1 f, Eq1 g, Eq a) => Eq ((f :.: g) a) where
+    Comp x == Comp y = eq1 (fmap Apply x) (fmap Apply y)
+
+instance (Functor f, Ord1 f, Ord1 g, Ord a) => Ord ((f :.: g) a) where
+    compare (Comp x) (Comp y) = compare1 (fmap Apply x) (fmap Apply y)
+
+instance (Functor f, Read1 f, Read1 g, Read a) => Read ((f :.: g) a) where
+    readsPrec = readsData $ readsUnary1 "Comp" (Comp . fmap getApply)
+
+instance (Functor f, Show1 f, Show1 g, Show a) => Show ((f :.: g) a) where
+    showsPrec d (Comp x) = showsUnary1 "Comp" d (fmap Apply x)
+
+instance (Functor f, Eq1 f, Eq1 g) => Eq1 (f :.: g) where eq1 = (==)
+instance (Functor f, Ord1 f, Ord1 g) => Ord1 (f :.: g) where
+    compare1 = compare
+instance (Functor f, Read1 f, Read1 g) => Read1 (f :.: g) where
+    readsPrec1 = readsPrec
+instance (Functor f, Show1 f, Show1 g) => Show1 (f :.: g) where
+    showsPrec1 = showsPrec
+#endif
 
 -- | Extract the contents of a 'Comp' value.
 unComp :: (f :.: g) p -> f (g p)
 unComp (Comp x) = x
-
