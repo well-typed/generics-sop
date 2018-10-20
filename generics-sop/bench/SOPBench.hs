be9380487c26312e24bd80cd164c629ef1cababf
Andres Loeh <andres@well-typed.com>
2018-10-20 09:20:37 +0200

    More benchmark grouping.

--- generics-sop/bench/SOPBench.hs
+++ generics-sop/bench/SOPBench.hs
@@ -9,48 +9,74 @@ main :: IO ()
 main =
   defaultMainWith defaultConfig
     [ bgroup "Roundtrip"
-      [ bench "S2 / GHCGeneric"     $ nf roundtrip (s2 :: S2 'GHCGeneric)
-      , bench "S2 / SOPGGP"         $ nf roundtrip (s2 :: S2 'SOPGGP    )
-      , bench "S2 / SOPTH"          $ nf roundtrip (s2 :: S2 'SOPTH     )
-      , bench "S20 / GHCGeneric"    $ nf roundtrip (s20 :: S20 'GHCGeneric)
-      , bench "S20 / SOPGGP"        $ nf roundtrip (s20 :: S20 'SOPGGP    )
-      , bench "S20 / SOPTH"         $ nf roundtrip (s20 :: S20 'SOPTH     )
-      , bench "PB2 / GHCGeneric"    $ nf roundtrip (pb2 :: PB2 'GHCGeneric)
-      , bench "PB2 / SOPGGP"        $ nf roundtrip (pb2 :: PB2 'SOPGGP    )
-      , bench "PB2 / SOPTH"         $ nf roundtrip (pb2 :: PB2 'SOPTH     )
+      [ bgroup "S2"
+        [ bench "GHCGeneric"     $ nf roundtrip (s2 :: S2 'GHCGeneric)
+        , bench "SOPGGP"         $ nf roundtrip (s2 :: S2 'SOPGGP    )
+        , bench "SOPTH"          $ nf roundtrip (s2 :: S2 'SOPTH     )
+        ]
+      , bgroup "S20"
+        [ bench "GHCGeneric"    $ nf roundtrip (s20 :: S20 'GHCGeneric)
+        , bench "SOPGGP"        $ nf roundtrip (s20 :: S20 'SOPGGP    )
+        , bench "SOPTH"         $ nf roundtrip (s20 :: S20 'SOPTH     )
+        ]
+      , bgroup "PB2"
+        [ bench "GHCGeneric"    $ nf roundtrip (pb2 :: PB2 'GHCGeneric)
+        , bench "SOPGGP"        $ nf roundtrip (pb2 :: PB2 'SOPGGP    )
+        , bench "SOPTH"         $ nf roundtrip (pb2 :: PB2 'SOPTH     )
+        ]
       ]
     , bgroup "Eq"
-      [ bench "S2 / GHCDeriving"           $ nf ((==) s2) (s2 :: S2 'GHCDeriving)
-      , bench "S2 / SOPGGP"                $ nf ((==) s2) (s2 :: S2 'SOPGGP     )
-      , bench "S2 / SOPTH"                 $ nf ((==) s2) (s2 :: S2 'SOPTH      )
-      , bench "S20 / GHCDeriving"          $ nf ((==) s20) (s20 :: S20 'GHCDeriving)
-      , bench "S20 / SOPGGP"               $ nf ((==) s20) (s20 :: S20 'SOPGGP     )
-      , bench "S20 / SOPTH"                $ nf ((==) s20) (s20 :: S20 'SOPTH      )
-      , bench "PB2 / GHCDeriving"          $ nf ((==) pb2) (pb2 :: PB2 'GHCDeriving)
-      , bench "PB2 / SOPGGP"               $ nf ((==) pb2) (pb2 :: PB2 'SOPGGP     )
-      , bench "PB2 / SOPTH"                $ nf ((==) pb2) (pb2 :: PB2 'SOPTH      )
-      , bench "Tree / GHCDeriving"         $ nf ((==) tree) (tree :: Tree 'GHCDeriving)
-      , bench "Tree / SOPGGP"              $ nf ((==) tree) (tree :: Tree 'SOPGGP     )
-      , bench "Tree / SOPTH"               $ nf ((==) tree) (tree :: Tree 'SOPTH      )
-      , bench "Tree large / GHCDeriving"   $ nf ((==) tree_large) (tree_large :: Tree 'GHCDeriving)
-      , bench "Tree large / SOPGGP"        $ nf ((==) tree_large) (tree_large :: Tree 'SOPGGP     )
-      , bench "Tree large / SOPTH"         $ nf ((==) tree_large) (tree_large :: Tree 'SOPTH      )
+      [ bgroup "S2"
+        [ bench "GHCDeriving"           $ nf ((==) s2) (s2 :: S2 'GHCDeriving)
+        , bench "SOPGGP"                $ nf ((==) s2) (s2 :: S2 'SOPGGP     )
+        , bench "SOPTH"                 $ nf ((==) s2) (s2 :: S2 'SOPTH      )
+        ]
+      , bgroup "S20"
+        [ bench "GHCDeriving"          $ nf ((==) s20) (s20 :: S20 'GHCDeriving)
+        , bench "SOPGGP"               $ nf ((==) s20) (s20 :: S20 'SOPGGP     )
+        , bench "SOPTH"                $ nf ((==) s20) (s20 :: S20 'SOPTH      )
+        ]
+      , bgroup "PB2"
+        [ bench "GHCDeriving"          $ nf ((==) pb2) (pb2 :: PB2 'GHCDeriving)
+        , bench "SOPGGP"               $ nf ((==) pb2) (pb2 :: PB2 'SOPGGP     )
+        , bench "SOPTH"                $ nf ((==) pb2) (pb2 :: PB2 'SOPTH      )
+        ]
+      , bgroup "Tree"
+        [ bench "GHCDeriving"         $ nf ((==) tree) (tree :: Tree 'GHCDeriving)
+        , bench "SOPGGP"              $ nf ((==) tree) (tree :: Tree 'SOPGGP     )
+        , bench "SOPTH"               $ nf ((==) tree) (tree :: Tree 'SOPTH      )
+        ]
+      , bgroup "Tree large"
+        [ bench "GHCDeriving"   $ nf ((==) tree_large) (tree_large :: Tree 'GHCDeriving)
+        , bench "SOPGGP"        $ nf ((==) tree_large) (tree_large :: Tree 'SOPGGP     )
+        , bench "SOPTH"         $ nf ((==) tree_large) (tree_large :: Tree 'SOPTH      )
+        ]
       ]
     , bgroup "Show"
-      [ bench "S2 / GHCDeriving"         $ nf show (s2 :: S2 'GHCDeriving)
-      , bench "S2 / SOPGGP"              $ nf show (s2 :: S2 'SOPGGP     )
-      , bench "S2 / SOPTH"               $ nf show (s2 :: S2 'SOPTH      )
-      , bench "S20 / GHCDeriving"        $ nf show (s20 :: S20 'GHCDeriving)
-      , bench "S20 / SOPGGP"             $ nf show (s20 :: S20 'SOPGGP     )
-      , bench "S20 / SOPTH"              $ nf show (s20 :: S20 'SOPTH      )
-      , bench "PB2 / GHCDeriving"        $ nf show (pb2 :: PB2 'GHCDeriving)
-      , bench "PB2 / SOPGGP"             $ nf show (pb2 :: PB2 'SOPGGP     )
-      , bench "PB2 / SOPTH"              $ nf show (pb2 :: PB2 'SOPTH      )
-      , bench "Tree / GHCDeriving"       $ nf show (tree :: Tree 'GHCDeriving)
-      , bench "Tree / SOPGGP"            $ nf show (tree :: Tree 'SOPGGP     )
-      , bench "Tree / SOPTH"             $ nf show (tree :: Tree 'SOPTH      )
-      , bench "Tree large / GHCDeriving" $ nf show (tree_large :: Tree 'GHCDeriving)
-      , bench "Tree large / SOPGGP"      $ nf show (tree_large :: Tree 'SOPGGP     )
-      , bench "Tree large / SOPTH"       $ nf show (tree_large :: Tree 'SOPTH      )
+      [ bgroup "S2"
+        [ bench "GHCDeriving"         $ nf show (s2 :: S2 'GHCDeriving)
+        , bench "SOPGGP"              $ nf show (s2 :: S2 'SOPGGP     )
+        , bench "SOPTH"               $ nf show (s2 :: S2 'SOPTH      )
+        ]
+      , bgroup "S20"
+        [ bench "GHCDeriving"        $ nf show (s20 :: S20 'GHCDeriving)
+        , bench "SOPGGP"             $ nf show (s20 :: S20 'SOPGGP     )
+        , bench "SOPTH"              $ nf show (s20 :: S20 'SOPTH      )
+        ]
+      , bgroup "PB2"
+        [ bench "GHCDeriving"        $ nf show (pb2 :: PB2 'GHCDeriving)
+        , bench "SOPGGP"             $ nf show (pb2 :: PB2 'SOPGGP     )
+        , bench "SOPTH"              $ nf show (pb2 :: PB2 'SOPTH      )
+        ]
+      , bgroup "Tree"
+        [ bench "GHCDeriving"       $ nf show (tree :: Tree 'GHCDeriving)
+        , bench "SOPGGP"            $ nf show (tree :: Tree 'SOPGGP     )
+        , bench "SOPTH"             $ nf show (tree :: Tree 'SOPTH      )
+        ]
+      , bgroup "Tree large"
+        [ bench "GHCDeriving" $ nf show (tree_large :: Tree 'GHCDeriving)
+        , bench "SOPGGP"      $ nf show (tree_large :: Tree 'SOPGGP     )
+        , bench "SOPTH"       $ nf show (tree_large :: Tree 'SOPTH      )
+        ]
       ]
     ]
