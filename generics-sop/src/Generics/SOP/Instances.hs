75cb8510a93167c38258dc56eb341cc196b8081a
Andres Loeh <andres@well-typed.com>
2021-04-10 09:09:51 +0200

    Changes for ghc-9.2 compatibility.

--- generics-sop/src/Generics/SOP/Instances.hs
+++ generics-sop/src/Generics/SOP/Instances.hs
@@ -206,7 +206,9 @@ deriveGeneric ''Data.Semigroup.Max -- new
 deriveGeneric ''Data.Semigroup.First -- new
 deriveGeneric ''Data.Semigroup.Last -- new
 deriveGeneric ''Data.Semigroup.WrappedMonoid -- new
+#if !MIN_VERSION_base(4,16,0)
 deriveGeneric ''Data.Semigroup.Option -- new
+#endif
 deriveGeneric ''Data.Semigroup.Arg -- new
 
 -- From Data.Version:
